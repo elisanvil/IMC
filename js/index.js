e610d8d96a7fc7d127bdc2abba2321e5a5fb8f17
Elizabeth Sanchez <iza_707@hotmail.com>
2018-05-15 03:23:30 -0500

    Agregando categorias de IMC

--- js/index.js
+++ js/index.js
@@ -2,16 +2,51 @@ function calcularIMC(){
   var peso;
   var estatura;
   var resultadoIMC;
+  var comentario;
 
   peso = document.getElementById("peso").value;
   estatura = document.getElementById("estatura").value;
   
   if(estatura != "" && peso != "") {
-
     estatura = parseInt(estatura) / 100;
     resultadoIMC = peso / Math.pow(estatura,2);
     document.getElementById("imc").value = resultadoIMC.toFixed(2);
-
   }
+
+  if(resultadoIMC <= 15) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Severamente bajo peso";
+  
+  } else if(resultadoIMC > 15 && resultadoIMC <= 16) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Muy bajo peso";
+
+  } else if(resultadoIMC > 16 && resultadoIMC <= 18.5) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Bajo peso";
+
+  } else if(resultadoIMC > 18.5 && resultadoIMC <= 25) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Peso saludable";
+  
+  } else if(resultadoIMC > 25 && resultadoIMC <= 30) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Sobrepeso";
+
+  } else if(resultadoIMC > 30 && resultadoIMC <= 35) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Moderadamente obeso";
+
+  } else if(resultadoIMC > 35 && resultadoIMC <= 40) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Severamente obeso";
+
+  } else if(resultadoIMC > 40 && resultadoIMC <= 45) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Muy severamente obeso";
+
+  } else if(resultadoIMC > 45 && resultadoIMC <= 50) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Mórbidamente obeso";
+
+  } else if(resultadoIMC > 50 && resultadoIMC <= 60) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Super obeso";
+
+  } else if(resultadoIMC > 60) {
+    comentario = "Tu IMC es " + resultadoIMC.toFixed(2) + " - Hiper obeso";
+  }
+
+  document.getElementById("comentario").innerHTML = comentario;
   
 }
